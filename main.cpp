5391779babb0b425d783a59f4ba10ec2145f43f2
lrivasc4 <160422579+lrivasc4@users.noreply.github.com>
2024-05-08 22:01:46 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tarea_grupal2
+//
+//  Created by Luis Rivas on 8/05/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
